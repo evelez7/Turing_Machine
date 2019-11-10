c286acb2e39b06a2f15eae7c9463864e09653c83
Erick <erickvelez7@gmail.com>
2019-11-10 00:06:03 -0800

    Abstract anonymous namespace from configuration.h

--- src/configuration.h
+++ src/configuration.h
@@ -5,20 +5,20 @@
  */
 
 #ifndef CONFIGURATIONS_H
-#define CONFIGUATIONS_H
+#define CONFIGURATIONS_H
 
 #include <experimental/filesystem>
 #include <string>
+#include "turing_machine.h"
 
 //NOTE: will need to refactor once C++17-compliant compiler is used
-//TODO: get C++17 compliant compiler
+//TODO: get C++17 compliant compiler, dont like experimental
 //NOTE: Xcode beta seems to be only reliable solution while on macos15 beta
 namespace fs = std::experimental::filesystem;
 
-namespace configuration {
-    namespace {
-        void openFile(fs::path filePath);
-    }
+namespace config {
+    void configure_machine(Turing_Machine machine, std::string file_path);
+    void configure_machine(Turing_Machine machine, fs::path file_path);
 }
 
 #endif
